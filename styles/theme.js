f10f5f8c54fec2a2d58ac0e03954432ed6dd7c27
Stephen <stephen@rutherford.dev>
2021-03-04 00:33:45 +0000

    Merge Hotfix

--- styles/theme.js
+++ styles/theme.js
@@ -1,16 +1,5 @@
 import { extendTheme } from "@chakra-ui/react"
 
-<<<<<<< HEAD
-const colors = {
-	transparent: "transparent",
-	black: "#000",
-	white: "#fff",
-}
-
-const customTheme = extendTheme({ colors })
-
-export default customTheme
-=======
 const config = {
 	initialColorMode: "light",
 	useSystemColorMode: false,
@@ -23,4 +12,3 @@ const config = {
 const theme = extendTheme({ config })
 
 export default theme
->>>>>>> 85cee068644f3dbd47908abe26faa2f8af904e07
